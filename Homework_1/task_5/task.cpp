8c7f7010d3e9181a04b19d638988dc1c86ff81d5
Данилова Анастасия <nastdan07@mail.ru>
2025-10-28 21:12:39 +0300

    Add files via upload

---
+++ Homework_1/task_5/task.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+int main() {
+	int x;
+	int y;
+	int res = 1;
+
+	std::cin >> x;
+
+	std::vector<int> mas;
+    for (int i = 0; i < x; ++i) {
+        std::cin >> y;
+        mas.push_back(y);
+    }
+
+	std::sort(mas.begin(), mas.end());
+
+	for (size_t i = 0; i < mas.size() - 1; ++i) {
+		if (mas[i] == mas[i + 1]) {
+			++res;
+		}
+	}
+
+	std::cout << res;
+}
