0e0ad77d0aca545a6ef1bd2f71b135e074159487
Данилова Анастасия <nastdan07@mail.ru>
2025-10-29 16:29:08 +0300

    Add files via upload

---
+++ Homework_1/task_7/task2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main() {
+	int x, y;
+	int res = -1;
+
+	std::cin >> x >> y;
+
+	for (x; x < y; ++x) {
+		if (x % 7 == 0) {
+			res = x;
+		}
+	}
+
+	std::cout << res;
+}
