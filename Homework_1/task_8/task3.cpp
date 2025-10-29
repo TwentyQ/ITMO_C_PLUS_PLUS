c0ca09d174c622ddaae9c45cf6e48708856aacd0
Данилова Анастасия <nastdan07@mail.ru>
2025-10-29 17:03:20 +0300

    Add files via upload

---
+++ Homework_1/task_8/task3.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+int main() {
+	std::string x;
+	int i;
+
+	std::cin >> x;
+
+	std::vector<char> mas;
+
+	for (char i : x) {
+		if (i == 'G') {
+			mas.push_back('C');
+		}
+		if (i == 'T') {
+			mas.push_back('A');
+		}
+		if (i == 'C') {
+			mas.push_back('G');
+		}
+		if (i == 'A') {
+			mas.push_back('T');
+		}
+	}
+
+	for (char y : mas) {
+		std::cout << y;
+	}
+}
